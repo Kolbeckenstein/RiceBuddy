25d510c40e552021a18120b0199b48f5795700be
Michael Kolbeck <michael.b.kolbeck@gmail.com>
2016-07-26 13:32:27 -0400

    Create ReadXML.java

---
+++ ReadXML.java
@@ -0,0 +1,18 @@
+package riceBuddy;
+
+import java.io.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+public class ReadXML {
+	//local variables
+	File grainDataFile;
+	
+	//constructor
+	public ReadXML(File givenFileAddress){
+		this.grainDataFile = givenFileAddress;
+	}
+	
+	//public methods
+	
+}
